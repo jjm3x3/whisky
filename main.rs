d9fb4f9b9b9c41c12cdd6aa5b3a36d42087193b6
Jacob Meixner <jjm3333@gmail.com>
2017-05-27 13:04:51 -0700

    Try logging to a file

--- main.rs
+++ main.rs
@@ -1,16 +1,10 @@
 use std::{thread, time};
-// use std::fs::File;
-// use std::io::prelude::*;
-// use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use std::net::{TcpListener, TcpStream};
 
 fn main () {
-    // let path = Path::new("foo.txt");
-    // // does not open in append mode
-    // let mut file = match File::create(&path) {
-    //     Err(why) => panic!("Couldn't read file: {}", why),
-    //     Ok(f) => f
-    // };
 
     let listener = match TcpListener::bind("127.0.0.1:80"){
         Ok(listener) => listener,
@@ -29,26 +23,23 @@ fn main () {
 
 }
 
-static mut is_busy: bool = false;
 fn handle_client(stream: TcpStream) {
-    unsafe {
-        if is_busy {
-            println!("i am already busy i should just return");
-            return
-        } else {
-            is_busy = true
-        }
-    }
     let ten_millis = time::Duration::from_millis(1000);
     //let now = time::Instant::now();
     println!("started");
+    let path = Path::new("foo.txt");
+    // does not open in append mode
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("Couldn't read file: {}", why),
+        Ok(f) => f
+    };
     for i in 0..10 {
         thread::sleep(ten_millis);
-        // match file.write_all(b"Woke up for a sec\n") {
-        //     Ok(worked) => worked,
-        //     Err(why) => panic!("Couldn't write to file: {}", why)
+        match file.write_all(b"Woke up for a sec\n") {
+            Ok(worked) => worked,
+            Err(why) => panic!("Couldn't write to file: {}", why)
                 
-        // };
+        };
         println!("wake up check number {}", i);
     }
     // println!("Do something with a steam")

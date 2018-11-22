4df066337b0bad9bce035b92dfc756394ffadbdb
Jacob Meixner <jjm3333@gmail.com>
2018-11-22 15:20:57 -0800

    Update clinet to work with server for debugging purposes

--- client.rs
+++ client.rs
@@ -2,12 +2,17 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 
 fn main() {
-    let mut stream = match TcpStream::connect("127.0.0.1:80") {
+    let mut stream = match TcpStream::connect("127.0.0.1:9080") {
         Ok(stream) => stream,
         Err(e) => panic!("Error establishing a connection: {}",e)
     };
 
-    stream.write(&[1]);
+    let _message = "Hi fam".as_bytes();
+
+    match stream.write(_message){
+        Ok(_) => println!("Message sent"),
+        Err(e) => panic!("Unable to send message: {}", e)
+    };
 }
 
     
